268ac3613d5557d69a69f8351b584ca9cdbb99c2
Valentina Vanu <vanuvalentina16@gmail.com>
2021-04-18 22:03:15 +0100

    WIP:  added some monkeys in the slider

--- src/component/slider-menu/slider-menu.component.jsx
+++ src/component/slider-menu/slider-menu.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Divider from "@material-ui/core/Divider";
@@ -6,36 +6,49 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
+import { monkeyList } from './config'
+
 
 import * as SS from './slider-menu.style'
 
 const SliderMenu = ({ show }) => {
+  const [selectedMonkey, setSelectedMonkey] = useState()
   const classes = SS.useStyles();
 
+  const handleSelectedMonkey = (monkeyName) => {
+    setSelectedMonkey(monkeyName)
+  }
+  
   return (
     <SS.StyledSSPaper show={show}>
       <List className={classes.root}>
-        <ListItem alignItems="flex-start">
-          <ListItemText
-            primary="Brunch this weekend?"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  Ali Connors
-                </Typography>
-                {" — I'll be in your neighborhood doing errands this…"}
-              </React.Fragment>
-            }
-          />
-          <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          </ListItemAvatar>
-        </ListItem>
+        {monkeyList.map(({name, path, description})=> {
+          return(
+            <Fragment key={name}>
+              <ListItem onClick={() => handleSelectedMonkey(name)} alignItems="flex-start">
+                <ListItemText
+                  primary={name}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        className={classes.inline}
+                        color="textPrimary"
+                      >
+                        {description}
+                      </Typography>
+                      {"---Click on dart monkey and place on map"}
+                    </React.Fragment>
+                  }
+                />
+                <ListItemAvatar>
+                  <Avatar alt={name} src={path} />
+                </ListItemAvatar>
+              </ListItem>
+            </Fragment>
+          )
+        })}
         <Divider variant="inset" component="li" />
       </List>
     </SS.StyledSSPaper>

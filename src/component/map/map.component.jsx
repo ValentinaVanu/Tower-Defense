b0cb470c4361eb029882d203c284f15a866233e7
Valentina Vanu <vanuvalentina16@gmail.com>
2021-04-18 21:14:05 +0100

    feat: add multiple monkeys on map this closes #7

--- src/component/map/map.component.jsx
+++ src/component/map/map.component.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { StyledMap } from './map.style';
+import { StyledMap, XYCell, XYMap } from './map.style';
 
 const Map = ({ children }) => {
+  const XYmap = [...Array(100).keys()]
   return (
-    <StyledMap elevation={8}>{children}</StyledMap>
+    // <StyledMap elevation={8}>{children}</StyledMap>
+    <StyledMap>
+      <XYMap>
+        {XYmap.map(cell => {
+          return (
+            <XYCell>{cell}{children}</XYCell>
+          )
+        })}
+      </XYMap>
+    </StyledMap>
   )
 };
 

ed3fd31c53e456768f195d88d5416d6dc77f39b1
ValentinaVanu <vanuvalentina16@gmail.com>
2021-06-01 09:15:43 +0100

    WIP: ops

--- src/component/map/map.component.jsx
+++ src/component/map/map.component.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import { StyledMap, XYCell, XYMap } from './map.style';
+import React from "react";
+// import { useDispatch } from 'react-redux';
+// import { setMonkeyCoordinateAction, setPlacedMonkeyAction } from '../../store/monkey/monkey.action';
+import { StyledMap, XYCell, XYMap } from "./map.style";
 
 import { useDroppable } from "@dnd-kit/core";
 
-
-const Map = ({id, children}) => {
-
-
+const Map = ({ id, children }) => {
   const { isOver, setNodeRef } = useDroppable({
     id: id,
   });
+  console.log(id);
   const style = {
-    color: isOver ? 'green' : undefined,
+    color: isOver ? "green" : undefined,
   };
 
   return (
-    <StyledMap elevation={8}>
-      <XYMap ref={setNodeRef} style={style}>
-        {children}
-      </XYMap>
-    </StyledMap>
-  )
+    <XYCell ref={setNodeRef} style={style}>
+      {children}
+    </XYCell>
+  );
 };
 
 export { Map };

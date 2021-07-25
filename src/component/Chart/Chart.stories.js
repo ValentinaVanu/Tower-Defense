13cade7a8c39d63b528ef855fecc1a0ddc9617b5
Valentina Vanu <valentina.vanu@1account.net>
2021-07-25 22:59:08 +0100

    WIP: profile chart

---
+++ src/component/Chart/Chart.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ProfileChart } from './Chart'
+
+export default {
+  title: 'Components/ProfileChart',
+  component: ProfileChart,
+}
+
+export const Default = (args) => <ProfileChart {...args}/>
+
+Default.args = {
+  win: 12,
+  loss: 4,
+}
